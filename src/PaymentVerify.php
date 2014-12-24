e2d41b5d3b8cafbd2a9e9979bfae3ce9ebd2c184
Tantacula <bonesbrain@gmail.com>
2014-12-24 08:01:06 +0500

    Добавлена функция проверки ЭЦП (смотреть в примерах).

--- src/PaymentVerify.php
+++ src/PaymentVerify.php
@@ -65,7 +65,7 @@ class PaymentVerify {
      * @return bool
      */
     public function isPaymentAccepted () {
-        return ( strtolower( $this->getOrderState() ) === 'accepted' );
+        return ( strtolower( $this->getOrderState() ) == 'accepted' );
     }
 
     /**
@@ -81,5 +81,68 @@ class PaymentVerify {
     public function getSellerPurse () {
         return $this->getResponseValue( 'WMI_MERCHANT_ID' );
     }
+
+    /**
+     * @param string   $signatureMethod
+     * @param string   $secret
+     * @param string   $encoding
+     *
+     * @return bool
+     *
+     * На данный момент у единой кассы заметен глюк: при отправке WMI_DESCRIPTION, содержащем двойные кавычки в теле обратно
+     * придет урезанный до этих кавычек WMI_DESCRIPTION, однако хэш будет посчитан по необрезанному параметру.
+     * Сейчас если хотите использовать подпись, то кавычек можно лишь избегать.
+     * Если это поправят - отпишите мне и уберите этот комментарий коммитом, буду признателен.
+     *
+     */
+    public function checkSignature($signatureMethod = NULL, $secret = NULL, $encoding = 'UTF-8') {
+
+        if (is_null($signatureMethod)) {
+            return TRUE;
+        }
+
+        if (($signatureMethod == 'md5' OR $signatureMethod == 'sha1') AND !is_null($secret)) {
+
+            $fields = $_POST;
+            if (!is_array($fields) or !isset($fields) or is_null($fields)) {
+                return FALSE;
+            }
+
+            foreach($fields as $name => $value)
+            {
+                if ($name != "WMI_SIGNATURE") $params[$name] = $value;
+            }
+
+            uksort($params, "strcasecmp"); $values = "";
+
+            foreach($params as $name => $value)
+            {
+                if ($encoding != 'windows-1251') {
+
+                    /*$value = mb_convert_encoding($value, 'windows-1251');*/
+
+                }
+                $values .= $value;
+            }
+
+            // Формирование подписи для сравнения ее с параметром WMI_SIGNATURE
+
+            $signature = '';
+
+            if ($signatureMethod == 'md5') {
+                $signature = base64_encode(pack("H*", md5($values . $secret)));
+            }
+            if ($signatureMethod == 'sha1') {
+                $signature = base64_encode(pack("H*", sha1($values . $secret)));
+            }
+
+            if ($signature == $fields["WMI_SIGNATURE"])
+            {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+
 
 }
